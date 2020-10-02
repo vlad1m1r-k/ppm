c3b11e2c9d85ea2770bd7945174b0f32e7192d6c
Vladimir <vova.kvz3@gmail.com>
2020-10-02 16:42:28 +0300

    ""

---
+++ src/main/Vue/components/mainBlock.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Page Content
+  </div>
+</template>
+
+<script>
+export default {
+  name: "mainBlock"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main/Vue/controller.js
@@ -0,0 +1,9 @@
+import MainBlock from './components/mainBlock.vue'
+
+const vm = new Vue({
+    el: '#app',
+    components: {
+        // 'login-cmp': LoginCmp,
+        'main-block': MainBlock
+    }
+});
