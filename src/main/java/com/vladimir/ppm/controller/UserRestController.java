6a1ec32fe15bf9468ebfc158565944485f605e00
Vladimir <vova.kvz3@gmail.com>
2020-11-03 17:48:56 +0200

    ""

--- src/main/java/com/vladimir/ppm/controller/UserRestController.java
+++ src/main/java/com/vladimir/ppm/controller/UserRestController.java
@@ -1,10 +1,13 @@
 package com.vladimir.ppm.controller;
 
 import com.vladimir.ppm.service.CryptoProviderService;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/user")
@@ -16,9 +19,14 @@ public class UserRestController {
     }
 
     @PostMapping("/login")
-    public boolean login(@RequestParam String key, @RequestParam String data) {
+    public boolean login(@RequestParam String key, @RequestParam String data, HttpServletRequest request) {
+        JSONObject json = new JSONObject(cryptoProviderService.decrypt(key, data));
+        String login = json.getString("login");
+        String password = json.getString("password");
+        String publicKeyPEM = json.getString("publicKey");
+        System.out.println(request.getRemoteAddr());
+        System.out.println(request.getHeader("User-Agent"));
         //TODO
-        cryptoProviderService.decrypt(key, data);
         return true;
     }
 }
