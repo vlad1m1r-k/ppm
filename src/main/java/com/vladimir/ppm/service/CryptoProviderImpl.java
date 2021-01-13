34236e9c80af38fbcc6961eba3ffd32608888118
Vladimir <vova.kvz3@gmail.com>
2021-01-13 19:33:34 +0200

    ""

--- src/main/java/com/vladimir/ppm/service/CryptoProviderImpl.java
+++ src/main/java/com/vladimir/ppm/service/CryptoProviderImpl.java
@@ -7,7 +7,6 @@ import com.vladimir.ppm.dto.PublicKeyDto;
 import org.apache.commons.lang3.ArrayUtils;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
-import org.hibernate.annotations.Synchronize;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -40,12 +39,13 @@ import java.util.Map;
 
 @Service
 public class CryptoProviderImpl implements CryptoProvider {
+    private final String RSACIPHER = "RSA/ECB/PKCS1Padding";
+    private final String AESCIPHER = "AES/CBC/PKCS7Padding";
+    private final String PROVIDER = "BC";
     private KeyPair keyPair;
     private Long keyPairExpireDate; //TODO renew keypair
     private SecretKeySpec tokenAESKey;
     private SecretKeySpec dbAESKey;
-    private Cipher rsaCipher;
-    private Cipher aesCipher;
     SecureRandom random = new SecureRandom();
 
     @Value("${keyLifeTimeDays}")
@@ -55,7 +55,7 @@ public class CryptoProviderImpl implements CryptoProvider {
     public void init() {
         Security.addProvider(new BouncyCastleProvider());
         try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA", "BC");
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA", PROVIDER);
             keyPairGenerator.initialize(2048, random);
             keyPair = keyPairGenerator.generateKeyPair();
             keyPairExpireDate = System.currentTimeMillis() + (long) keyLifeTimeDays * 24 * 60 * 60 * 1000;
@@ -68,9 +68,7 @@ public class CryptoProviderImpl implements CryptoProvider {
             byte[] dbKey = {25, 126, -91, 78, 87, 110, -25, -27, 6, 121, 44, 96, -63, 17, 32, -69, -29, -8, 51, -12, 80, -44, 61, 108, 120, 36, -55, -86, 2, -117, -119, -123};
             dbAESKey = new SecretKeySpec(dbKey, "AES");
 
-            rsaCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding", "BC");
-            aesCipher = Cipher.getInstance("AES/CBC/PKCS7Padding", "BC");
-        } catch (NoSuchAlgorithmException | NoSuchProviderException | NoSuchPaddingException e) {
+        } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
             e.printStackTrace();
         }
     }
@@ -93,16 +91,18 @@ public class CryptoProviderImpl implements CryptoProvider {
     }
 
     @Override
-    synchronized public CryptoDto encrypt(String publicKey, String data) {
+    public CryptoDto encrypt(String publicKey, String data) {
         byte[] aesKey = new byte[32];
         byte[] aesIv = new byte[16];
         random.nextBytes(aesKey);
         random.nextBytes(aesIv);
         byte[] encryptedData = null;
         try {
+            Cipher aesCipher = Cipher.getInstance(AESCIPHER, PROVIDER);
             aesCipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(aesKey, "AES"), new IvParameterSpec(aesIv));
             encryptedData = aesCipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
-        } catch (InvalidKeyException | InvalidAlgorithmParameterException | IllegalBlockSizeException | BadPaddingException e) {
+        } catch (InvalidKeyException | InvalidAlgorithmParameterException | IllegalBlockSizeException |
+                BadPaddingException | NoSuchAlgorithmException | NoSuchProviderException | NoSuchPaddingException e) {
             e.printStackTrace();
         }
         String aesKeyB64 = Base64.getEncoder().encodeToString(aesKey);
@@ -119,10 +119,11 @@ public class CryptoProviderImpl implements CryptoProvider {
             KeyFactory keyFactory = KeyFactory.getInstance("RSA");
             X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyBytes);
             PublicKey frontPublicKey = keyFactory.generatePublic(keySpec);
+            Cipher rsaCipher = Cipher.getInstance(RSACIPHER, PROVIDER);
             rsaCipher.init(Cipher.ENCRYPT_MODE, frontPublicKey);
             encryptedAesKey = rsaCipher.doFinal(aesKeyBundle.toString().getBytes());
-        } catch (NoSuchAlgorithmException | InvalidKeySpecException | InvalidKeyException |
-                IllegalBlockSizeException | BadPaddingException e) {
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException | InvalidKeyException | IllegalBlockSizeException |
+                BadPaddingException | NoSuchProviderException | NoSuchPaddingException e) {
             e.printStackTrace();
         }
         return CryptoDto.builder()
@@ -132,49 +133,56 @@ public class CryptoProviderImpl implements CryptoProvider {
     }
 
     @Override
-    synchronized public String decrypt(String key, String data) {
+    public String decrypt(String key, String data) {
         byte[] keyBytes = Base64.getDecoder().decode(key);
         byte[] dataBytes = Base64.getDecoder().decode(data);
         String decryptedString = "";
         try {
+            Cipher rsaCipher = Cipher.getInstance(RSACIPHER, PROVIDER);
             rsaCipher.init(Cipher.DECRYPT_MODE, keyPair.getPrivate());
             JSONObject json = new JSONObject(new String(rsaCipher.doFinal(keyBytes)));
             byte[] aesKeyBytes = Base64.getDecoder().decode(json.getString("key"));
             byte[] aesIVBytes = Base64.getDecoder().decode(json.getString("iv"));
 
+            Cipher aesCipher = Cipher.getInstance(AESCIPHER, PROVIDER);
             aesCipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(aesKeyBytes, "AES"), new IvParameterSpec(aesIVBytes));
             decryptedString = new String(aesCipher.doFinal(dataBytes), StandardCharsets.UTF_8);
-        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException e) {
+        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException |
+                NoSuchAlgorithmException | NoSuchProviderException | NoSuchPaddingException e) {
             e.printStackTrace();
         }
         return decryptedString;
     }
 
     @Override
-    synchronized public String encryptToken(Token token) {
+    public String encryptToken(Token token) {
         String encryptedB64Token = null;
         byte[] iv = generateIV();
         IvParameterSpec aesIv = new IvParameterSpec(iv);
         try {
+            Cipher aesCipher = Cipher.getInstance(AESCIPHER, PROVIDER);
             aesCipher.init(Cipher.ENCRYPT_MODE, tokenAESKey, aesIv);
             byte[] encryptedToken = aesCipher.doFinal(token.toJson().getBytes(StandardCharsets.UTF_8));
             encryptedToken = insertIv(iv, encryptedToken);
             encryptedB64Token = Base64.getEncoder().encodeToString(encryptedToken);
-        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException e) {
+        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException |
+                NoSuchAlgorithmException | NoSuchProviderException | NoSuchPaddingException e) {
             e.printStackTrace();
         }
         return encryptedB64Token;
     }
 
     @Override
-    synchronized public Token decryptToken(String token) {
+    public Token decryptToken(String token) {
         String tokenStr = "";
         Map<String, byte[]> tokenMap = extractIv(Base64.getDecoder().decode(token));
         IvParameterSpec aesIv = new IvParameterSpec(tokenMap.get("iv"));
         try {
+            Cipher aesCipher = Cipher.getInstance(AESCIPHER, PROVIDER);
             aesCipher.init(Cipher.DECRYPT_MODE, tokenAESKey, aesIv);
             tokenStr = new String(aesCipher.doFinal(tokenMap.get("data")), StandardCharsets.UTF_8);
-        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException e) {
+        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException |
+                NoSuchAlgorithmException | NoSuchProviderException | NoSuchPaddingException e) {
             e.printStackTrace();
         }
         JSONObject json = new JSONObject(tokenStr);
@@ -204,10 +212,12 @@ public class CryptoProviderImpl implements CryptoProvider {
         IvParameterSpec aesIv = new IvParameterSpec(iv);
         byte[] encryptedText = new byte[0];
         try {
+            Cipher aesCipher = Cipher.getInstance(AESCIPHER, PROVIDER);
             aesCipher.init(Cipher.ENCRYPT_MODE, dbAESKey, aesIv);
             encryptedText = aesCipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
             encryptedText = insertIv(iv, encryptedText);
-        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException e) {
+        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException |
+                NoSuchAlgorithmException | NoSuchProviderException | NoSuchPaddingException e) {
             e.printStackTrace();
         }
         return encryptedText;
@@ -219,9 +229,11 @@ public class CryptoProviderImpl implements CryptoProvider {
         Map<String, byte[]> entryMap = extractIv(bytes);
         IvParameterSpec aesIv = new IvParameterSpec(entryMap.get("iv"));
         try {
+            Cipher aesCipher = Cipher.getInstance(AESCIPHER, PROVIDER);
             aesCipher.init(Cipher.DECRYPT_MODE, dbAESKey, aesIv);
             text = new String(aesCipher.doFinal(entryMap.get("data")), StandardCharsets.UTF_8);
-        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException e) {
+        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException |
+                NoSuchAlgorithmException | NoSuchProviderException | NoSuchPaddingException e) {
             e.printStackTrace();
         }
         return text;
---
+++ src/test/java/com/vladimir/ppm/service/CryptoProviderImplTest.java
@@ -0,0 +1,79 @@
+package com.vladimir.ppm.service;
+
+import com.vladimir.ppm.dto.CryptoDto;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CryptoProviderImpl.class)
+public class CryptoProviderImplTest {
+    @Autowired
+    private CryptoProviderImpl cpi;
+    private List<CryptoDto> list = new CopyOnWriteArrayList();
+
+    @Ignore
+    @Test
+    public void testMultiThreadEncryption() throws InterruptedException, ExecutionException {
+        ExecutorService service = Executors.newFixedThreadPool(50);
+        List<EncryptWorker> encryptWorkers = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            encryptWorkers.add(new EncryptWorker(i));
+        }
+        List<Future<Void>> futures = service.invokeAll(encryptWorkers);
+        for (Future<Void> future : futures) {
+            future.get();
+        }
+        service.shutdown();
+//        service.awaitTermination(10000L, TimeUnit.MILLISECONDS);
+    }
+
+    private class EncryptWorker implements Callable<Void> {
+        private final SecureRandom random = new SecureRandom();
+        private final int number;
+
+        public EncryptWorker(int number) {
+            this.number = number;
+        }
+
+        @Override
+        public Void call() {
+            String publicKey = "-----BEGIN PUBLIC KEY-----\r\n" +
+                    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsGPbpLkD/wattp0U5GQF\r\n" +
+                    "PBCy458XWcQnuqLTIcwLII2qsQ4XXoMu1HSq8KOhXn7K0IKOLcpkTMSrl9yTcKKD\r\n" +
+                    "yGbH+UyO1u0YfRFbUSJwAb8zdDFrajC0PihP8hc0WUxpmoVr61/XdnZheJzOypfB\r\n" +
+                    "juNY5HoAT7ZDXBPPRGD1yO0TWC5ZbYg2kIIupp6cgpZ9V768Mln7/HKSjenvwwlY\r\n" +
+                    "9gLFwbGgPnvuoU/2StQSRet8qBJaaDja3x8xYs+nHz0PLmHwRZxEt1ruTXukDkU1\r\n" +
+                    "MFg/T9QXV+FKoTwSYvdMghhDKxYabzsWE+4bOU0RkHe0POLt15W2OtbA8CTEukY3\r\n" +
+                    "uwIDAQAB\r\n" +
+                    "-----END PUBLIC KEY-----";
+            for (int i = 0; i < 10; i++) {
+                byte[] rndBytes = new byte[32];
+                random.nextBytes(rndBytes);
+                String str = new String(rndBytes);
+//                System.out.println("Worker #" + number + " try " + i);
+                try {
+                    list.add(cpi.encrypt(publicKey, str));
+                } catch (Exception e) {
+                    System.out.println("EXCEPTION in Worker #" + number + " try " + i);
+//                    e.printStackTrace();
+                }
+//                throw new IllegalArgumentException();
+            }
+            return null;
+        }
+    }
+}
