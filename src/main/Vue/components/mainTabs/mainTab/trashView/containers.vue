d4600e12b1b26430ef631d9ec57f2e84b7a59754
Vladimir <vova.kvz3@gmail.com>
2021-03-15 17:28:38 +0200

    "deleted containers"

--- src/main/Vue/components/mainTabs/mainTab/trashView/containers.vue
+++ src/main/Vue/components/mainTabs/mainTab/trashView/containers.vue
@@ -2,16 +2,47 @@
     <table class="table table-bordered table-striped table-sm">
         <thead class="tab-header-area">
         <tr>
+            <th>
+                <button class="btn btn-sm btn-link" @click="setSort('name')">{{ language.data.div1 }}</button>
+            </th>
+            <th>{{ language.data.iv7 }}</th>
+            <th>{{ language.data.iv8 }}</th>
+            <th><button class="btn btn-sm btn-link" @click="setSort('createdDate')">{{ language.data.div2 }}</button></th>
+            <th><button class="btn btn-sm btn-link" @click="setSort('createdBy')">{{ language.data.div3 }}</button></th>
+            <th><button class="btn btn-sm btn-link" @click="setSort('editedDate')">{{ language.data.div4 }}</button></th>
+            <th><button class="btn btn-sm btn-link" @click="setSort('editedBy')">{{ language.data.div5 }}</button></th>
+            <th><button class="btn btn-sm btn-link" @click="setSort('deletedDate')">{{ language.data.div6 }}</button></th>
+            <th><button class="btn btn-sm btn-link" @click="setSort('deletedBy')">{{ language.data.div7 }}</button></th>
+            <th></th>
             <th></th>
         </tr>
         </thead>
         <tbody>
-
+            <tr v-for="cont in containers">
+                <td>{{ cont.name }}</td>
+                <td>
+                    <select class="form-control-sm">
+                        <option selected>{{ language.data.iv7 }} {{ cont.notes.length }}</option>
+                        <option v-for="note in cont.notes" disabled>{{ note.name }}</option>
+                    </select>
+                </td>
+                <td>
+                    <select class="form-control-sm">
+                        <option selected>{{ language.data.iv8 }} {{ cont.passwords.length }}</option>
+                        <option v-for="pwd in cont.passwords" disabled>{{ pwd.name }}</option>
+                    </select>
+                </td>
+                <td>{{ cont.createdDate }}</td>
+                <td>{{ cont.createdBy }}</td>
+                <td>{{ cont.editedDate }}</td>
+                <td>{{ cont.editedBy }}</td>
+                <td>{{ cont.deletedDate }}</td>
+                <td>{{ cont.deletedBy }}</td>
+                <td>restore</td>
+                <td>delete</td>
+            </tr>
         </tbody>
     </table>
-    <div v-for="cont in containers">
-        {{ cont.id }}
-    </div>
 </template>
 
 <script>
@@ -27,7 +58,7 @@ export default {
             language: this.$root.$data.language,
             tokenProvider: this.$root.$data.tokenProvider,
             containers: [],
-            sort: new Sort("name", "desc"),
+            sort: new Sort("name", "asc"),
         }
     },
     methods: {
@@ -48,7 +79,13 @@ export default {
             } catch (e) {
                 this.eventHub.emit("show-msg", this.errorParser(e));
             }
+        },
+        setSort(field) {
+            this.sort.setField(field);
+            this.getContainers();
         }
+        //TODO restore containers
+        //TODO remove containers
     },
     mounted() {
         this.getContainers();
