b8af0d582aa7df024451f0501ba96e7774cff4c9
V <V@DESKTOP-NM0KC4U>
2026-02-26 18:14:32 +0200

    1.4.0

--- src/main/Vue/components/mainTabs/mainTab/trashView/items.vue
+++ src/main/Vue/components/mainTabs/mainTab/trashView/items.vue
@@ -2,23 +2,21 @@
     <div>
         <div class="decor-tr" @click="showNotes = !showNotes">
             {{ items.notes.length }} {{ language.data.iv7 }}
-            <span class="btn-dc text-success" :title="language.data.cm7" @click.stop="restoreNotes">&#x21ba;</span>
-            <span class="btn-dc" :title="language.data.cm5" @click.stop="removeNotes">&#x1f5d1;</span>
+            <button class="btn-img redo" :title="language.data.cm7" @click.stop="restoreNotes"></button>
+            <button class="btn-img rmv" :title="language.data.cm5" @click.stop="removeNotes"></button>
         </div>
         <div v-show="showNotes">
-            <table class="table table-bordered table-striped table-sm">
-                <thead class="tab-header-area">
+            <table class="table">
+                <thead>
                 <tr>
                     <th><input type="checkbox" @change="checkToggle($event.target.checked, 'notes')"></th>
-                    <th>
-                        <button class="btn btn-sm btn-link" @click="setNotesSort('name')">{{ language.data.div1 }}</button>
-                    </th>
-                    <th><button class="btn btn-sm btn-link" @click="setNotesSort('createdDate')">{{ language.data.div2 }}</button></th>
-                    <th><button class="btn btn-sm btn-link" @click="setNotesSort('createdBy')">{{ language.data.div3 }}</button></th>
-                    <th><button class="btn btn-sm btn-link" @click="setNotesSort('editedDate')">{{ language.data.div4 }}</button></th>
-                    <th><button class="btn btn-sm btn-link" @click="setNotesSort('editedBy')">{{ language.data.div5 }}</button></th>
-                    <th><button class="btn btn-sm btn-link" @click="setNotesSort('deletedDate')">{{ language.data.div6 }}</button></th>
-                    <th><button class="btn btn-sm btn-link" @click="setNotesSort('deletedBy')">{{ language.data.div7 }}</button></th>
+                    <th><button class="btn link" @click="setNotesSort('name')">{{ language.data.div1 }}</button></th>
+                    <th><button class="btn link" @click="setNotesSort('createdDate')">{{ language.data.div2 }}</button></th>
+                    <th><button class="btn link" @click="setNotesSort('createdBy')">{{ language.data.div3 }}</button></th>
+                    <th><button class="btn link" @click="setNotesSort('editedDate')">{{ language.data.div4 }}</button></th>
+                    <th><button class="btn link" @click="setNotesSort('editedBy')">{{ language.data.div5 }}</button></th>
+                    <th><button class="btn link" @click="setNotesSort('deletedDate')">{{ language.data.div6 }}</button></th>
+                    <th><button class="btn link" @click="setNotesSort('deletedBy')">{{ language.data.div7 }}</button></th>
                     <th></th>
                     <th></th>
                 </tr>
@@ -31,23 +29,21 @@
         </div>
         <div class="decor-tr" @click="showPass = !showPass">
             {{ items.passwords.length }} {{ language.data.iv8 }}
-            <span class="btn-dc text-success" :title="language.data.cm7" @click.stop="restorePass">&#x21ba;</span>
-            <span class="btn-dc" :title="language.data.cm5" @click.stop="removePass">&#x1f5d1;</span>
+            <button class="btn-img redo" :title="language.data.cm7" @click.stop="restorePass"></button>
+            <button class="btn-img rmv" :title="language.data.cm5" @click.stop="removePass"></button>
         </div>
         <div v-show="showPass">
-            <table class="table table-bordered table-striped table-sm">
-                <thead class="tab-header-area">
+            <table class="table">
+                <thead>
                 <tr>
                     <th><input type="checkbox" @change="checkToggle($event.target.checked, 'pass')"></th>
-                    <th>
-                        <button class="btn btn-sm btn-link" @click="setPwdSort('name')">{{ language.data.div1 }}</button>
-                    </th>
-                    <th><button class="btn btn-sm btn-link" @click="setPwdSort('createdDate')">{{ language.data.div2 }}</button></th>
-                    <th><button class="btn btn-sm btn-link" @click="setPwdSort('createdBy')">{{ language.data.div3 }}</button></th>
-                    <th><button class="btn btn-sm btn-link" @click="setPwdSort('editedDate')">{{ language.data.div4 }}</button></th>
-                    <th><button class="btn btn-sm btn-link" @click="setPwdSort('editedBy')">{{ language.data.div5 }}</button></th>
-                    <th><button class="btn btn-sm btn-link" @click="setPwdSort('deletedDate')">{{ language.data.div6 }}</button></th>
-                    <th><button class="btn btn-sm btn-link" @click="setPwdSort('deletedBy')">{{ language.data.div7 }}</button></th>
+                    <th><button class="btn link" @click="setPwdSort('name')">{{ language.data.div1 }}</button></th>
+                    <th><button class="btn link" @click="setPwdSort('createdDate')">{{ language.data.div2 }}</button></th>
+                    <th><button class="btn link" @click="setPwdSort('createdBy')">{{ language.data.div3 }}</button></th>
+                    <th><button class="btn link" @click="setPwdSort('editedDate')">{{ language.data.div4 }}</button></th>
+                    <th><button class="btn link" @click="setPwdSort('editedBy')">{{ language.data.div5 }}</button></th>
+                    <th><button class="btn link" @click="setPwdSort('deletedDate')">{{ language.data.div6 }}</button></th>
+                    <th><button class="btn link" @click="setPwdSort('deletedBy')">{{ language.data.div7 }}</button></th>
                     <th></th>
                     <th></th>
                 </tr>
@@ -60,24 +56,22 @@
         </div>
         <div class="decor-tr" @click="showFls = !showFls">
             {{ items.files.length }} {{ language.data.fl1 }}
-            <span class="btn-dc text-success" :title="language.data.cm7" @click.stop="restoreFiles">&#x21ba;</span>
-            <span class="btn-dc" :title="language.data.cm5" @click.stop="removeFiles">&#x1f5d1;</span>
+            <button class="btn-img redo" :title="language.data.cm7" @click.stop="restoreFiles"></button>
+            <button class="btn-img rmv" :title="language.data.cm5" @click.stop="removeFiles"></button>
         </div>
         <div v-show="showFls">
-            <table class="table table-bordered table-striped table-sm">
-                <thead class="tab-header-area">
+            <table class="table">
+                <thead>
                 <tr>
                     <th><input type="checkbox" @change="checkToggle($event.target.checked, 'files')"></th>
-                    <th>
-                        <button class="btn btn-sm btn-link" @click="setFlsSort('name')">{{ language.data.div1 }}</button>
-                    </th>
-                    <th><button class="btn btn-sm btn-link" @click="setFlsSort('size')">{{ language.data.div8 }}</button></th>
-                    <th><button class="btn btn-sm btn-link" @click="setFlsSort('createdDate')">{{ language.data.div2 }}</button></th>
-                    <th><button class="btn btn-sm btn-link" @click="setFlsSort('createdBy')">{{ language.data.div3 }}</button></th>
-                    <th><button class="btn btn-sm btn-link" @click="setFlsSort('editedDate')">{{ language.data.div4 }}</button></th>
-                    <th><button class="btn btn-sm btn-link" @click="setFlsSort('editedBy')">{{ language.data.div5 }}</button></th>
-                    <th><button class="btn btn-sm btn-link" @click="setFlsSort('deletedDate')">{{ language.data.div6 }}</button></th>
-                    <th><button class="btn btn-sm btn-link" @click="setFlsSort('deletedBy')">{{ language.data.div7 }}</button></th>
+                    <th><button class="btn link" @click="setFlsSort('name')">{{ language.data.div1 }}</button></th>
+                    <th><button class="btn link" @click="setFlsSort('size')">{{ language.data.div8 }}</button></th>
+                    <th><button class="btn link" @click="setFlsSort('createdDate')">{{ language.data.div2 }}</button></th>
+                    <th><button class="btn link" @click="setFlsSort('createdBy')">{{ language.data.div3 }}</button></th>
+                    <th><button class="btn link" @click="setFlsSort('editedDate')">{{ language.data.div4 }}</button></th>
+                    <th><button class="btn link" @click="setFlsSort('editedBy')">{{ language.data.div5 }}</button></th>
+                    <th><button class="btn link" @click="setFlsSort('deletedDate')">{{ language.data.div6 }}</button></th>
+                    <th><button class="btn link" @click="setFlsSort('deletedBy')">{{ language.data.div7 }}</button></th>
                     <th></th>
                     <th></th>
                 </tr>
